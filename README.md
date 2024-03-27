fc4df2078ac32962789161d2d0c705cc4b406d32
emannuel <emannuelloxt@hotmail.com>
2024-03-27 12:46:38 -0400

    fix: maintain plus char

--- README.md
+++ README.md
@@ -14,9 +14,11 @@ npm install shopify-slugify
 ```
 
 ## Usage in JavaScript/Node.js
+
 Once the package is installed, you can use it in your JavaScript/Node.js code. Import the slugify function and call it with a string to generate a slug:
+
 ```js
-import { slugify } from 'shopify-slugify';
+import { slugify } from "shopify-slugify";
 
 const inputString = "This is a Sample String!";
 const slug = slugify(inputString);
@@ -24,12 +26,11 @@ console.log(slug);
 // Output: this-is-a-sample-string
 ```
 
-
 The slugify function takes a string as an argument and returns a slugified version of the input string. It performs the following transformations:
 
 - Converts the string to lowercase.
 - Replaces spaces with hyphens.
 - Replaces special characters with their corresponding ASCII equivalents.
-- Removes non-word characters (except for º).
+- Removes non-word characters (except for º and +).
 - Replaces multiple hyphens with a single hyphen.
 - Trims hyphens from the start and end of the resulting string.
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export function slugify(text: string): string {
   const from =
-    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìıİłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;";
+    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìıİłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż+·/_,:;";
   const to =
     "aaaaaaaaaacccddeeeeeeeegghiiiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz--------------";
   const textToCompare = new RegExp(from.split("").join("|"), "g");
@@ -11,7 +11,7 @@ export function slugify(text: string): string {
     .replace(/\s+/g, "-") // Replace spaces with -
     .replace(textToCompare, (c) => to.charAt(from.indexOf(c))) // Replace special characters
     .replace(/[&.]/g, "-") // Replace DOT with -
-    .replace(/[^\w-º]+/g, "") // Remove all non-word characters, except for º and -
+    .replace(/[^\w-º+]+/g, "") // Remove all non-word characters, except for º, + and -
     .replace(/--+/g, "-") // Replace multiple - with single -
     .replace(/^-+/, "") // Trim - from start of text
     .replace(/-+$/, ""); // Trim - from end of text
